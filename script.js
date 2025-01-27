75931067c6f7a54371f9c224776a425bfc54c546
icepick15 <iamicepick@gmail.com>
2025-01-27 01:32:39 -0800

    Tailwind Index Page Project

--- script.js
+++ script.js
@@ -13,3 +13,27 @@ change_btn.addEventListener('click', () => {
         change_image.src = 'img/moon.svg'
     }
 })
+
+// Desktop
+
+let change_btn2 = document.getElementById('change_btn2')
+let change_image2 = document.getElementById('change_image2')
+
+change_btn2.addEventListener('click', () => {
+    html.classList.toggle('dark');
+
+    // change image source base on preference
+
+    if(change_image2.src.includes('img/moon.svg')){
+        change_image2.src = 'img/sun.svg';
+    } else{
+        change_image2.src = 'img/moon.svg'
+    }
+})
+
+let main_menu = document.getElementById('main_menu')
+let mobile_btn = document.getElementById('mobile_btn')
+
+mobile_btn.addEventListener('click', () => {
+    main_menu.classList.toggle('hidden');
+})
